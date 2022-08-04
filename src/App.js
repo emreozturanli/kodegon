4a6b080f3520f7dfa54c320723462a051bb6a1b8
emre <emreozturanli1@gmail.com>
2022-08-04 14:07:31 +0300

    video component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import NavTabs from "./components/NavTabs";
 import { SliderComponent } from "./components/SliderComponent";
-
+import Video from "./components/Video";
 
 function App() {
   return (
     <div className="App">
      <SliderComponent/>
      <NavTabs/>
+     <Video/>
     </div>
   );
 }
---
+++ src/components/Video.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <div className='video-container'>
+        <iframe width="100%" height="211" src="https://www.youtube.com/embed/Y1qQZbTF8iQ" title="Earth Views: Earth From Space Seen From The ISS" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    </div>
+  )
+}
+
+export default Video
